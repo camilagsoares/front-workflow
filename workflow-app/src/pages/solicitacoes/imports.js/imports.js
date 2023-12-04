9a2779653fa719b25855b1770949c9b91792d239
camilagsoares <camila.soares19@hotmail.com>
2023-12-04 10:11:13 -0300

    Criado arquivo imports

---
+++ workflow-app/src/pages/solicitacoes/imports.js/imports.js
@@ -0,0 +1,77 @@
+// imports.js
+import PropTypes from 'prop-types';
+import Button from '@mui/material/Button';
+import AddCircle from '@mui/icons-material/AddCircleOutline';
+import FilterAlt from '@mui/icons-material/FilterAltOutlined';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Divider from '@mui/material/Divider';
+import Lista from '../components/lista/index';
+import ModalForm from '../components/modal_form';
+import { useState, useContext } from 'react';
+import DrawerView from '../components/drawer_view';
+import ModalAtualizarEtapasProjeto from '../components/modal_atualizarEtapasProjeto';
+import ModalAdicionarProcessoLicitatorio from '../components/modal_adicionar_processo_licitatorio';
+import ModalEditarProjeto from '../components/modal_editar_projeto';
+import InputAdornment from '@mui/material/InputAdornment';
+import { AuthContext } from "../../../contexts/auth.context"
+import ModalConcluirProjeto from "../components/modal_concluir_projeto"
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
+import Grid from '@mui/material/Grid';
+import { useApiRequestGet } from '../../../services/api';
+import ModalPrioridadeProjeto from "../components/modal_prioridade_projeto/index"
+import { useEffect } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import CircularProgress from '@mui/material/CircularProgress';
+import Autocomplete from '@mui/material/Autocomplete';
+import "../styles.css"
+import Paper from '@mui/material/Paper';
+import { styled } from '@mui/material/styles';
+import { NavLink } from 'react-router-dom';
+import Grow from '@mui/material/Grow';
+
+export {
+  PropTypes,
+  Button,
+  AddCircle,
+  FilterAlt,
+  Box,
+  Typography,
+  TextField,
+  Divider,
+  Lista,
+  ModalForm,
+  useState,
+  useContext,
+  DrawerView,
+  ModalAtualizarEtapasProjeto,
+  ModalAdicionarProcessoLicitatorio,
+  ModalEditarProjeto,
+  InputAdornment,
+  AuthContext,
+  ModalConcluirProjeto,
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Grid,
+  useApiRequestGet,
+  ModalPrioridadeProjeto,
+  useEffect,
+  useForm,
+  Controller,
+  yup,
+  yupResolver,
+  CircularProgress,
+  Autocomplete,
+  Paper,
+  styled,
+  NavLink,
+  Grow,
+};
