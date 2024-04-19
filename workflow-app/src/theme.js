d17a68901889e2e61f101841b119aed35426ad84
camilagsoares <camila.soares19@hotmail.com>
2024-04-19 08:24:36 -0300

    change theme

--- workflow-app/src/theme.js
+++ workflow-app/src/theme.js
@@ -1,6 +1,5 @@
 import { createTheme } from '@mui/material';
 
-// Aqui é possível criar um tema padrão para o MUI
 const theme = createTheme({
   typography: {
     fontFamily: 'Roboto, sans-serif',
